930fc57da2a54ef23496c16369f77f4ae76294de
adel djidjik <ea_djidjik@esi.dz>
2021-03-20 21:01:35 +0100

    loaded data from store

--- src/pages/GlobalRecords/index.js
+++ src/pages/GlobalRecords/index.js
@@ -1,22 +1,53 @@
 // Globals
-import React from "react";
+import React, {useEffect} from "react";
 
 // Components
 import { Record } from "components/Record";
 
 // Misc
-import { data } from "components/Record/data";
+import { useSelector, useDispatch } from "react-redux";
+import { mockFetch } from "../../util/mockFetch"
+
 
 // Component
 function GlobalRecords() {
+  const dispatch= useDispatch()
+  const global = useSelector(state=>state.records)
+  useEffect(()=>{
+    dispatch({
+      type:'LOAD_DATA'
+    })
+    mockFetch().then(res=>{
+      dispatch({
+        type:'LOAD_SUCCESS',
+        payload:res
+      })
+    })
+    .catch((err)=>{
+      dispatch({
+        type:'LOAD_FAIL',
+        payload:[err]
+      })
+      console.log("mock err", err);
+    })
+  }, [])
+  
   return (
     <div className="aura-page aura-global_records">
       <h1>Top Records of 2020</h1>
 
       <div className="aura-page-content">
-        {data.map((record) => {
+        {
+          global.loading && <p>Loading</p>
+        }
+        {
+          !!global.errors.length && <p>Error</p>
+        }
+        {
+         !global.loading && !!!global.errors.length && global?.records?.map((record) => {
           return <Record key={record.id} data={record} />;
-        })}
+        })
+        }
       </div>
     </div>
   );
--- src/services/records/index.js
+++ src/services/records/index.js
@@ -1,1 +1,11 @@
-export const records = () => null;
+export const records = (state={records:[], loading:false, errors:[]}, action) => {
+    switch(action.type){
+        case 'LOAD_DATA':
+            return {...state, loading:true}
+        case 'LOAD_SUCCESS':
+            return {...state, loading:false, records:action.payload}
+        case 'LOAD_FAIL':
+            return {...state, loading:false, errors:action.payload}
+        default : return state
+    }
+};

a3f61b598ee53a749d8713388a4d0ac1bada0da7
adel djidjik <ea_djidjik@esi.dz>
2021-03-21 22:20:39 +0100

    test length

--- src/services/records/test.js
+++ src/services/records/test.js
@@ -8,6 +8,27 @@ describe("records service", () => {
 
   // Root reducer
   it("has records reducer", () => {
-    expect(getState().records).toBe(null);
+    expect(getState().records).not.toBe(null)
+  });
+
+  it("has records data on the store", () => {
+
+    // simulate dispatch adding data records to redux store
+    dispatch({
+      type: 'LOAD_SUCCESS',
+      payload: { data:[{
+        id: 123,
+        artist: {
+          name: 'Some artist',
+          id: 456,
+        },
+        album: {
+          title: 'Some record',
+          imageSrc: '/some/asset/path.jpg'
+        }
+      }],count:1 }
+    })
+    expect(getState().records.records.length).toBe(1)
+    expect(getState().records.records.length).toEqual(getState().records.total) // length of records array should equal to total and === 1
   });
 });
